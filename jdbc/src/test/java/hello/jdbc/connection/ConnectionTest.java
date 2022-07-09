7b6a1033b51180058e8200bccd7edd422e8d1c68
JungwooSim <cscd053@gmail.com>
2022-07-09 18:43:49 +0900

    2. 커넥션풀과 데이터소스 이해 / DataSource 예제2 - 커넥션 풀

--- jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -4,6 +4,7 @@ import static hello.jdbc.connection.ConnectionConst.PASSWORD;
 import static hello.jdbc.connection.ConnectionConst.URL;
 import static hello.jdbc.connection.ConnectionConst.USERNAME;
 
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -29,6 +30,19 @@ public class ConnectionTest {
     // DriverManagerDataSource - 항상 새로운 커넥션을 획득
     DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
     useDataSource(dataSource);
+  }
+
+  @Test
+  void dataSourceConnectionPool() throws SQLException, InterruptedException {
+    HikariDataSource dataSource = new HikariDataSource();
+    dataSource.setJdbcUrl(URL);
+    dataSource.setUsername(USERNAME);
+    dataSource.setPassword(PASSWORD);
+    dataSource.setMaximumPoolSize(10);
+    dataSource.setPoolName("MyPool");
+
+    useDataSource(dataSource);
+    Thread.sleep(1000); // pool 에 추가되는 것을 확인하기 위한 목적
   }
 
   private void useDataSource(DataSource dataSource) throws SQLException {
